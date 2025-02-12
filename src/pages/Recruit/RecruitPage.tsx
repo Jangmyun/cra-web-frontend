8787d5a9c9f95a6ddbf9cb169f7595cafad2062a
min06150315 <min06150315@gmail.com>
2025-02-12 17:38:27 +0900

    [KB] feat[#215]: 리크루팅 페이지 디자인 약간

--- src/pages/Recruit/RecruitPage.tsx
+++ src/pages/Recruit/RecruitPage.tsx
@@ -18,13 +18,17 @@ export default function RecruitPage() {
     <div className={styles['recruit-container']}>
       <div className={styles['recruit-main']}>
         <HeaderIntro />
-        <hr />
         <div className={styles['recruit-banner']}>
-          <p id={styles['title']}>2025 CRA</p>
+          <p id={styles['title']}>2025-1 CRA</p>
           <p id={styles['title']}>RECRUITMENT</p>
           <p id={styles['content']}>
             CRA는 함께 성장 할 25-1 기수 동아리원을 모집합니다.
           </p>
+          <div className={styles['recruit-apply']}>
+            <Link to="https://docs.google.com/forms/d/e/1FAIpQLSf5uTQbDr7i9WjPfI61hMJ_PqDS1Of_fZRNpD8MRzlvnYFsKA/closedform">
+              <button className={styles['button-style']}>지원하기</button>
+            </Link>
+          </div>
           <div className={styles['vector']} onClick={scrollToSection}>
             <img src={Vector2} />
             <img src={Vector} />
