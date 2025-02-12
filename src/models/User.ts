aa971d1820c44ff04e3d9237d8e6eb593fbcf326
min06150315 <min06150315@gmail.com>
2025-02-12 17:29:01 +0900

    [KB] feat[#215]: 유저 인터페이스 Model 제작

---
+++ src/models/User.ts
@@ -0,0 +1,20 @@
+interface User {
+  id?: number;
+  name: string;
+  email: string;
+  studentId: number;
+  term: string;
+  githubId: string;
+  delete?: true;
+  createdAt?: Date;
+  updatedAt?: Date;
+  imageUrl?: string;
+}
+
+interface UserPassWord {
+  username: string;
+  code: string;
+  password: string;
+}
+
+export type { User, UserPassWord };
