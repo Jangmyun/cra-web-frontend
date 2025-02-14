9213dbffee86381e562ac4ed51c8d5df124d0774
JunYoung Lee <120372886+juneyng@users.noreply.github.com>
2025-02-14 17:19:44 +0900

    [JY] feat[#186]: type 문제 해결

--- src/api/like.ts
+++ src/api/like.ts
@@ -1,7 +1,11 @@
 import { Board } from '~/models/Board';
 import { authClient } from './auth/authClient';
 
-export const createLike = async (id: number, isLike: boolean) => {
+export const createLike = async (
+  id: number,
+  userId: number,
+  isLike: boolean,
+) => {
   try {
     console.log('요청 URL:', `/board/like/${id}`, 'params:', { isLike });
 
