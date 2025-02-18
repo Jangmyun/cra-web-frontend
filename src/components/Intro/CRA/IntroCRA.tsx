0fe3212377b770c6d21ba8933a0138573bca520b
min06150315 <min06150315@gmail.com>
2025-02-19 05:53:24 +0900

    [KB] style[#247]: CRA 소개 구역 반응형 디자인 (모바일 까지)

--- src/components/Intro/CRA/IntroCRA.tsx
+++ src/components/Intro/CRA/IntroCRA.tsx
@@ -1,24 +1,56 @@
+import { useEffect, useState } from 'react';
 import styles from './IntroCRA.module.css';
 
 function IntroCRA() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 620);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 620);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return (
     <div className={styles.cra}>
-      <div className={styles.title}>
-        <span id={styles['cap-letter']}>C</span>
-        <p>omputer</p>
-        <span id={styles['cap-letter']}>R</span>
-        <p>esearch</p>
-        <span id={styles['cap-letter']}>A</span>
-        <p>ssoication</p>
-      </div>
-
+      {!isMobile ? (
+        <>
+          {/* Computer Research Assoication */}
+          <div className={styles.title}>
+            <span className={styles.CapLetter}>C</span>
+            <p>omputer</p>
+            <span className={styles.CapLetter}>R</span>
+            <p>esearch</p>
+            <span className={styles.CapLetter}>A</span>
+            <p>ssoication</p>
+          </div>
+        </>
+      ) : (
+        <>
+          <>
+            {/* Computer Research Assoication */}
+            <div className={styles.CRAtitle}>
+              <div className={styles.CRATitleContent}>
+                <span className={styles.CapLetter}>C</span>
+                <p>omputer</p>
+              </div>
+              <div className={styles.CRATitleContent}>
+                <span className={styles.CapLetter}>R</span>
+                <p>esearch</p>
+              </div>
+              <div className={styles.CRATitleContent}>
+                <span className={styles.CapLetter}>A</span>
+                <p>ssoication</p>
+              </div>
+            </div>
+          </>
+        </>
+      )}
       {/* CRA 소개 */}
       <div className={styles.description}>
-        <p>CRA는 한동대학교 전산 교육과정에 기초하여</p>
         <p>
+          CRA는 한동대학교 전산 교육과정에 기초하여
+          <br />
           한 분야에 국한되지 않는 신기술을 공부하고 습득한 지식과 기술을 통해
-        </p>
-        <p>
+          <br />
           <span>‘배워서 남주자’</span>를 실천하는 동아리입니다.
         </p>
       </div>
@@ -26,22 +58,25 @@ function IntroCRA() {
       {/* CRA 소개 카드 */}
       <div className={styles.content}>
         {/* CARD 1 */}
-        <div className={styles.card}>
-          <p id={styles['card-title']}>CRA가 창립한지</p>
-          <p id={styles['card-content']}>30년</p>
+        <div className={styles.Card}>
+          <p className={styles.CardTitle}>CRA가 창립한지</p>
+          <p className={styles.CardContent}>30년</p>
         </div>
 
         {/* CARD 2 */}
-        <div className={styles.card}>
-          <p id={styles['card-title']}>출시 서비스</p>
-          <p id={styles['card-content']}>?개</p>
+        <div className={styles.Card}>
+          <p className={styles.CardTitle}>출시 서비스</p>
+          <p className={styles.CardContent}>?개</p>
         </div>
 
         {/* CARD 3 */}
-        <div className={styles.card}>
-          <p id={styles['card-title']}>선배들과 함께하는</p>
-          <p id={styles['card-content2']}>정기적인</p>
-          <p id={styles['card-content2']}>교류활동</p>
+        <div className={styles.Card}>
+          <p className={styles.CardTitle}>선배들과 함께하는</p>
+          <p className={styles.CardContent2}>
+            정기적인
+            <br />
+            교류활동
+          </p>
         </div>
       </div>
     </div>
