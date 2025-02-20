1749382448994b3cfecb295a1c87c7b50084769a
min06150315 <min06150315@gmail.com>
2025-02-20 15:21:55 +0900

    [KB] style[#256]: 하브루타 드롭다운, 사이드바 추가

--- src/components/Havruta/HavrutaBoard/List/HavrutaBoardList.tsx
+++ src/components/Havruta/HavrutaBoard/List/HavrutaBoardList.tsx
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom';
 import { UseQueryResult } from '@tanstack/react-query';
 import { Havruta, HavrutaBoard } from '~/models/Havruta.ts';
-import Sidebar from './Sidebar/HavrutaSidebar.tsx';
+
+import SideBar from './Sidebar/HavrutaSidebar.tsx';
+import Dropdown from './Dropdown/HavrutaDropDown.tsx';
 import Pagination from '~/components/Pagination/Pagination.tsx';
 import HavrutaBoardItem from '~/components/Havruta/HavrutaBoard/Item/HavrutaBoardItem.tsx';
 import styles from './HavrutaBoardList.module.css';
@@ -26,46 +28,39 @@ export default function HavrutaBoardList({
   onHavrutaChange,
 }: HavrutaBoardListProps) {
   const renderBoardContent = () => {
-    if (havrutaBoardQuery.isLoading) {
+    if (havrutaBoardQuery.isLoading)
       return (
-        <div>
-          <div className={styles['board-wrapper']}>
-            데이터를 불러오는 중입니다...
-          </div>
+        <div className={styles.loading}>
+          <div className="LoadingSpinner"></div>
         </div>
       );
-    }
+    if (totalPages === 0)
+      return <div className={styles.noBoards}>현재 게시물이 없습니다.</div>;
 
-    if (havrutaBoardQuery.isError) {
-      return (
-        <div className={styles['board-wrapper']}>에러가 발생했습니다!</div>
-      );
-    }
+    if (havrutaBoardQuery.isError)
+      return <div className={styles.error}>에러가 발생했습니다!</div>;
 
     if (havrutaBoardQuery.isSuccess) {
-      return havrutaBoardQuery.data.length > 0 ? (
-        havrutaBoardQuery.data
-          .filter((havrutaBoard) => havrutaBoard.id !== undefined)
-          .map((havrutaBoard, index) => (
-            <div key={`havruta-${havrutaBoard.id}`}>
-              <div className={styles['board-wrapper']}>
-                <HavrutaBoardItem havrutaBoard={havrutaBoard} />
-              </div>
-              {index < havrutaBoardQuery.data.length - 1 && (
-                <div className={styles['divider']}></div>
-              )}
+      return havrutaBoardQuery.data
+        .filter((havrutaBoard) => havrutaBoard.id !== undefined)
+        .map((havrutaBoard, index) => (
+          <div key={`havruta-${havrutaBoard.id}`}>
+            <div className={styles['board-wrapper']}>
+              <HavrutaBoardItem havrutaBoard={havrutaBoard} />
             </div>
-          ))
-      ) : (
-        <div className={styles['board-wrapper']}>게시글이 없습니다.</div>
-      );
+            {index < havrutaBoardQuery.data.length - 1 && (
+              <div className={styles.divider}></div>
+            )}
+          </div>
+        ));
     }
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles.sidebar}>
-        <Sidebar
+      <h2 className={styles.title}>하브루타 게시판</h2>
+      <div className={styles.dropdown}>
+        <Dropdown
           havrutaQuery={havrutaQuery}
           selectedHavrutaId={selectedHavrutaId}
           onHavrutaChange={onHavrutaChange}
@@ -73,19 +68,28 @@ export default function HavrutaBoardList({
         />
       </div>
       <div className={styles.content}>
-        <h2 className={styles.title}>하브루타 게시판</h2>
-        <div className={styles.boardList}>{renderBoardContent()}</div>
-        <div className={styles['board-list-footer']}>
-          <div className={styles['spacer']}></div>
-          <Pagination
-            totalPages={totalPages}
-            currentPage={currentPage}
+        <div className={styles.sidebar}>
+          <SideBar
+            havrutaQuery={havrutaQuery}
+            selectedHavrutaId={selectedHavrutaId}
+            onHavrutaChange={onHavrutaChange}
             onPageChange={onPageChange}
           />
+        </div>
+        <div className={styles.boardContent}>
+          <div className={styles.boardList}>{renderBoardContent()}</div>
+          <div className={styles['board-list-footer']}>
+            <div className={styles.spacer}></div>
+            <Pagination
+              totalPages={totalPages}
+              currentPage={currentPage}
+              onPageChange={onPageChange}
+            />
 
-          <Link className={styles['write-link']} to="/havruta/write">
-            글쓰기
-          </Link>
+            <Link className={styles.WriteLink} to="/havruta/write">
+              글쓰기
+            </Link>
+          </div>
         </div>
       </div>
     </div>
