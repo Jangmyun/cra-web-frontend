55a990f0292aceb2801c4755741b7c82cdb1434e
JangSiYoung <chsy1894@handong.ac.kr>
2025-02-24 20:23:00 +0900

    [SY] feat[#317]: merge

--- src/pages/Recruit/RecruitPage.tsx
+++ src/pages/Recruit/RecruitPage.tsx
@@ -7,6 +7,53 @@ import A from '~/assets/images/RecruitImage/A.png?format=webp&as=srcset';
 import SCRETCH from '~/assets/images/RecruitImage/scretch.png?format=webp&as=srcset';
 import CONTENT from '~/assets/images/RecruitImage/content.png?format=webp&as=srcset';
 import styles from './RecruitPage.module.css';
+const clubContents = [
+  {
+    number: '01',
+    content: (
+      <>
+        다른 사람과 협력하며 공동의 목표를 <br />
+        이루고 서로 배우고 발전하며{' '}
+        <span style={{ color: 'var(--color-primary)' }}>함께 성장</span>
+        하고 싶은 사람
+      </>
+    ),
+  },
+  {
+    number: '02',
+    content: (
+      <>
+        어떤 문제든 두려움 없이 극복하려는 의지를 가지고
+        <br />
+        새로운 기회를 탐구하는 용기와{' '}
+        <span style={{ color: 'var(--color-primary)' }}>도전 의식</span>을 지닌
+        사람
+      </>
+    ),
+  },
+  {
+    number: '03',
+    content: (
+      <>
+        익숙하지 않은 새로운 지식을 접할 때도 적극적으로 받아들이고
+        <br />
+        실수를 통해 성장하며 자신의 능력을 확장하려고{' '}
+        <span style={{ color: 'var(--color-primary)' }}>노력</span>하는 사람
+      </>
+    ),
+  },
+  {
+    number: '04',
+    content: (
+      <>
+        자신이 맡은 일을 끝까지 책임지고 완수하며 <br />
+        팀과 조직을 위해 기여하고자 하는{' '}
+        <span style={{ color: 'var(--color-primary)' }}>책임감</span>이 강한
+        사람
+      </>
+    ),
+  },
+];
 
 export default function RecruitPage() {
   const recruitTalentRef = useRef<HTMLDivElement>(null);
@@ -123,49 +170,12 @@ export default function RecruitPage() {
         </h2>
 
         <div ref={recruitTalentRef} className={styles['recruit-talentDetail']}>
-          <div>
-            <span className={styles['number']}>01</span>
-            <p>
-              다른 사람과 협력하며 공동의 목표를 <br />
-              이루고 서로 배우고 발전하며{' '}
-              <span style={{ color: 'var(--color-primary)' }}>함께 성장</span>
-              하고 싶은 사람
-            </p>
-          </div>
-          <div>
-            <span className={styles['number']}>02</span>
-            <p>
-              어떤 문제든 두려움 없이 극복하려는 의지를 가지고
-              <br />
-              새로운 기회를 탐구하는 용기와{' '}
-              <span style={{ color: 'var(--color-primary)' }}>도전 의식</span>을
-              지닌 사람
-            </p>
-          </div>
-          <div>
-            <span className={styles['number']}>03</span>
-            <p>
-              익숙하지 않은 새로운 지식을 접할 때도 적극적으로 받아들이고
-              <br />
-              실수를 통해 성장하며 자신의 능력을 확장하려고{' '}
-              <span style={{ color: 'var(--color-primary)' }}>노력</span>하는
-              사람
-            </p>
-          </div>
-          <div>
-            <span className={styles['number']}>04</span>
-            <p>
-              다양한 경험과 지식을 습득하며{' '}
-              <span style={{ color: 'var(--color-primary)' }}>스스로 성장</span>
-              하는 동시에,
-              <br />
-              자신이 얻은 것들을 주변 사람들에게 아낌없이{' '}
-              <span style={{ color: 'var(--color-primary)' }}>
-                나눠줄 수 있는
-              </span>{' '}
-              사람
-            </p>
-          </div>
+          {clubContents.map((item, index) => (
+            <div key={index}>
+              <span className={styles['number']}>{item.number}</span>
+              <p>{item.content}</p>
+            </div>
+          ))}
         </div>
       </div>
 
