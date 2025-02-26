f171e3de671b20259571ca29265f99ea0f818b8f
min06150315 <min06150315@gmail.com>
2025-02-26 17:14:03 +0900

    [KB] super-hotfix: 배포 설정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import HeaderMain from './Main-Header/HeaderMain';
+// import HeaderMain from './Main-Header/HeaderMain';
 import { useLocation } from 'react-router-dom';
 
 // 현재 페이지가 Intro & Recruit 일 경우 Header_Intro 출력
@@ -8,5 +8,5 @@ export default function Header() {
   const location = useLocation();
   const isIntro = location.pathname === '/';
   const isRecruit = location.pathname === '/recruit';
-  return isIntro || isRecruit ? <></> : <HeaderMain />;
+  return isIntro || isRecruit ? <></> : <></>;
 }
